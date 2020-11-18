81b73cd7521453f0d71c37ae2eca35516178c38c
Brian Li <Brian.Li96@yahoo.com>
2020-11-18 15:50:03 -0600

    commit tree e34a1a5970887b84a50f22207e73f8c78cf29489

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,53 @@
+
 # your code goes here
+class Person
+  attr_reader :name
+  attr_accessor :bank_account, :happiness, :hygiene
+
+ def initialize (name)
+  @name = name
+  @bank_account = 25
+  @happiness = 8
+  @hygiene = 8
+ end 
+
+ def happiness
+
+  if @happiness > 10
+    @happiness = 10
+  elsif @happiness < 0    
+    @happiness = 0
+  end 
+  @happiness
+ end 
+
+  def hygiene
+
+    if @hygiene > 10
+      @hygiene = 10
+    elsif @hygiene < 0    
+      @hygiene = 0
+    end 
+    @hygiene
+  end 
+
+  def happy?
+    if @happiness > 7
+      return true 
+    else
+      false 
+    end 
+  end 
+
+  def clean?
+    if @hygiene >7
+      return true
+    else
+      false
+    end 
+  end 
+
+  def get_paid(salary)
+  end
+  
+end
